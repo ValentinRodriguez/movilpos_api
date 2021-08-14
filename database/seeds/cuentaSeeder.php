0b6802750a9b1889f32e50362700ae3079a4d74f
Jhonny Soto <jsoto@movilsoluciones.com.do>
2021-08-14 19:58:00 -0400

    actualiza

--- database/seeds/cuentaSeeder.php
+++ database/seeds/cuentaSeeder.php
@@ -9,13 +9,8 @@ class cuentaSeeder extends Seeder
     {
         $cgcatalogo = array(
             [
-<<<<<<< HEAD
-                'cuenta_no'          => '0',
-                'descripcion'        => 'prov-control',
-=======
                 'cuenta_no'          => 1,
                 'descripcion'        => 'cuenta control general',
->>>>>>> 4938678b91a33aaae72ecc72094c00df22b3d51d
                 'origen'             => 'deudor',
                 'nivel'              => 1,
                 'aplica_a'           => 1,
@@ -28,23 +23,13 @@ class cuentaSeeder extends Seeder
                 'selectivo_consumo'  => 'no',
                 'retencion'          => 'no',
                 'codigo_isr'         => 1,
-<<<<<<< HEAD
                 'usuario_creador'    => 'movilsoluciones',
-                'estado'             => 'inactivo',
-                'grupo'              => 'pasivo'
-            ],
-            [
-                'cuenta_no'          => '00',
-                'descripcion'        => 'prov-sub',
-=======
-                'usuario_creador'    => 'movilsoluciones',               
                 'estado'             => 'activo',
                 'grupo'              => 'pasivo'
             ],
             [
                 'cuenta_no'          => 11,
                 'descripcion'        => 'cuenta sub control general',
->>>>>>> 4938678b91a33aaae72ecc72094c00df22b3d51d
                 'origen'             => 'deudor',
                 'nivel'              => 2,
                 'aplica_a'           => 1,
@@ -57,23 +42,13 @@ class cuentaSeeder extends Seeder
                 'selectivo_consumo'  => 'no',
                 'retencion'          => 'no',
                 'codigo_isr'         => 1,
-<<<<<<< HEAD
                 'usuario_creador'    => 'movilsoluciones',
-                'estado'             => 'inactivo',
-                'grupo'              => 'pasivo'
-            ],
-            [
-                'cuenta_no'          => '000',
-                'descripcion'        => 'prov-aux',
-=======
-                'usuario_creador'    => 'movilsoluciones',               
                 'estado'             => 'activo',
                 'grupo'              => 'pasivo'
             ],
             [
                 'cuenta_no'          => 111,
                 'descripcion'        => 'cuenta auxiliar general',
->>>>>>> 4938678b91a33aaae72ecc72094c00df22b3d51d
                 'origen'             => 'deudor',
                 'nivel'              => 3,
                 'aplica_a'           => 11,
@@ -86,13 +61,8 @@ class cuentaSeeder extends Seeder
                 'selectivo_consumo'  => 'no',
                 'retencion'          => 'no',
                 'codigo_isr'         => 1,
-<<<<<<< HEAD
                 'usuario_creador'    => 'movilsoluciones',
-                'estado'             => 'inactivo',
-=======
-                'usuario_creador'    => 'movilsoluciones',               
                 'estado'             => 'activo',
->>>>>>> 4938678b91a33aaae72ecc72094c00df22b3d51d
                 'grupo'              => 'pasivo'
             ],
         );
